e3377af439168e8c1da7c9bb7dd7191e76d5e0f6
imShZH <zhihaoshen7@gmail.com>
2021-10-04 22:13:41 +0800

    feat: copy asset files to the output directory.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+    // Copy `assets/` to output directory
+    eleventyConfig.addPassthroughCopy({"assets": "assets"});
+
     // Set custom directories for input, output, includes, and data
     return {
         dir: {
