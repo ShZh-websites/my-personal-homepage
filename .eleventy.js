a7de4e33d76504fb0c54000592b8e95a29182b57
imShZH <zhihaoshen7@gmail.com>
2021-10-06 11:46:50 +0800

    feat(UI): extract biography into 11ty's data source.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,14 @@ module.exports = function(eleventyConfig) {
     // Copy `assets/` to output directory
     eleventyConfig.addPassthroughCopy({"assets": "assets"});
 
+    // Parse .txt file as data input
+    eleventyConfig.dataExtensions = new Map([
+        ["txt", function parse(str) {
+            // must return object, otherwise TemplateData cleanupData throws TypeError
+            return { text: str };
+        }],
+    ]);
+
     // Set custom directories for input, output, includes, and data
     return {
         dir: {
